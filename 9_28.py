caae7fa9135602d775ca1d9c427944d0d005cdfc
Fredchen <fredchen001@outlook.com>
2021-09-30 21:40:23 -0700

    first commit

---
+++ 9_28.py
@@ -0,0 +1,44 @@
+# print ("hello world")
+
+# print('The quick brown fox', 'jumps over', 'the lazy dog', sep=",")
+
+# name = input()
+# print(name)
+
+# name = input('please enter your name: ')
+# print('hello', name)
+
+# print("I\'m \"OK\"")
+
+# print('''line1 
+# ... line2
+# ... line3''')
+
+# 但事实上PI仍然是一个变量，Python根本没有任何机制保证PI不会被改变，所以，用全部大写的变量名表示常量只是一个习惯上的用法，如果你一定要改变变量PI的值，也没人能拦住你。
+
+# print(10/3)
+# print(10//3)
+# print(10%3)
+
+# print(ord('A'))
+# print(ord('中'))
+# print(chr(65))
+
+# print('Hello, %s' % 'world')
+# print('Hi, %s, you have $%d.' % ('Michael', 1000000))
+# %d	整数
+# %f	浮点数
+# %s	字符串
+# %x	十六进制整数
+
+# print('Age: %s. Gender: %s' % (25, True))
+# print( 'growth rate: %d %%' % 7)
+
+# r = 2.5
+# s = 3.14 * r ** 2
+# print(f'The area of a circle with radius {r} is {s:.2f}')
+
+classmate = ['Michael', 'Bob', 'Tracy']
+print(classmate[0])
+print(classmate[2])
+print(classmate[3])
