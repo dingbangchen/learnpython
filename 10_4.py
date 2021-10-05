e9aed78d6837a75c94ea8d64ecb56ffa773e877e
Fredchen <fredchen001@outlook.com>
2021-10-04 19:20:45 -0700

    10_4

---
+++ 10_4.py
@@ -0,0 +1,27 @@
+# print(abs(100))
+# print(abs(-20))
+# print(max(1,2,3,4))
+# print(max(1,3,4,-5))
+
+# print(int('123'))
+# print(int(12.34))
+# print(float('123'))
+# print(str(123))
+# print(str(100))
+# print(bool(1))
+# print(bool(''))
+# print(bool(2))
+
+# def my_abs(x):
+#     if x > 0:
+#         return x
+#     if x < 0:
+#         return -x
+# print(my_abs(-99))
+
+# def nop():
+#     pass
+# if age >= 18:
+#     pass
+# pass语句什么都不做，那有什么用？实际上pass可以用来作为占位符，比如现在还没想好怎么写函数的代码，就可以先放一个pass，让代码能运行起来。
+
