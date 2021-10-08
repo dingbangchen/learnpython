e3599fc9f6c6b6ad453394538e49d824959aa2ae
Fredchen <fredchen001@outlook.com>
2021-10-07 22:54:25 -0700

    1/7/4

--- 10_7.py
+++ 10_7.py
@@ -12,17 +12,17 @@
 # print(L[-2])
 # print(L[1:])
 
-#任意无符号的对象，以逗号隔开，默认为元组
-print('abc',-4.23e93,18+6.6j,'xyz')
-x,y = 1,2
-print('Value of x,y', x,y)
+# #任意无符号的对象，以逗号隔开，默认为元组
+# print('abc',-4.23e93,18+6.6j,'xyz')
+# x,y = 1,2
+# print('Value of x,y', x,y)
 
-#元组内置函数
-比较两个元组元素
-    cmp(tuple1,tuple2)
-计算元组元素个数
-    len(tuple)
-返回元组中元素最大值。
-    min(tuple)
-将列表转换为元组。
-    tuple(seq)
+# #元组内置函数
+# 比较两个元组元素
+#     cmp(tuple1,tuple2)
+# 计算元组元素个数
+#     len(tuple)
+# 返回元组中元素最大值。
+#     min(tuple)
+# 将列表转换为元组。
+#     tuple(seq)
