aa6b9994f56c0b790f1ae6fbea53c0454dc5ac74
Fredchen <fredchen001@outlook.com>
2021-10-31 22:13:24 -0700

    10.31.1

---
+++ 10_31.py
@@ -0,0 +1,61 @@
+# name = "Ada ada ADA"
+# print(name.title())
+# print(name.upper())
+# print(name.lower())
+
+
+# first_name = 'ada'
+# last_name = 'lovelace'
+# fullname = f"{first_name} {last_name}" 
+# # This string is call f-string, f is for format, because python formats the string by replacing the name of any variable
+# # in brace with its value
+# print(fullname)
+# # you can also use f-string to compose complete message using the information associated with a variable
+# print(f"Hello, {fullname.title()}") 
+# # 这里一定要注意方法后面的括号，否则就是指向内存空间
+# message = f"Hello, {fullname.title()}"
+# print(message)
+
+
+
+# # Adding white space
+# print("\tPython")
+# # print("Language:\nPython\nJave\nC")
+# favourite_language = 'python '
+# print(favourite_language.rstrip())
+# print(favourite_language)
+# favourite_language = favourite_language.rstrip()
+
+# favourite_language = ' python '
+# print(favourite_language.lstrip())
+# print(favourite_language.strip())
+
+# # message = "One of Python's strength"
+# # print(message)
+# # # message2= 'One of Python's strength'
+# # # print(message2)                This is wrong
+
+# # practice 2.3
+# name = 'Eric'
+# print(f"Hello {name}, would you like to learn some python today?")
+
+# #2.4
+# name2 = "rachel Du"
+# print(name2.lower())
+# print(name2.upper())
+# print(name2.title())
+
+# #2.5
+# famous_person = "Albert Einstein"
+# Quote = "A person blabla"
+# print(f"{famous_person} once said :{Quote}")
+
+
+
+# universal_age = 14_000_000_000 
+# print(universal_age)
+# 这里不能用逗号，否则就变成数组了
+
+# multiple assignment
+x,y,z = 1, 2, 4
+print(x, y, z)
