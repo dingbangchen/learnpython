40cc7d13d64b629da2763abf902096cc1eb8eeb7
Fredchen <fredchen001@outlook.com>
2021-11-09 20:02:00 -0800

    2

--- 11_9.py
+++ 11_9.py
@@ -55,10 +55,68 @@
 # print(6%3)
 # print(7%3)
 
-# EVEN AND ODD test
-number = input("tell me a number and I will tell you it is even or odd\n")
-number = int(number)
-if number%2 == 0:
-    print("This is a even number")
-else:
-    print("This is a odd number")   
+# # EVEN AND ODD test
+# number = input("tell me a number and I will tell you it is even or odd\n")
+# number = int(number)
+# if number%2 == 0:
+#     print("This is a even number")
+# else:
+#     print("This is a odd number")   
+
+
+
+# WHILE LOOP
+# current_number = 1
+# while current_number <= 5:
+#     print(current_number)
+#     current_number += 1
+
+# # let the user choose when to quit
+# prompt = '\nTell me something and i will repeat it back to you\n'
+# prompt += "Enter quit to end the program\n"
+# message = ""
+# while message != 'quit':
+#     message = input(prompt)
+#     if message != 'quit':
+#         print(message)                                #if the message is equal to 'quit', just end the if and while loop
+
+
+
+# # Using a flag
+# prompt = "Tell me something and I will repeat it for you\n"
+# prompt += "Enter quit to end the program\n"
+# active = True
+# while active:
+#     message = input(prompt)
+#     if message != 'quit':
+#         print(message)
+#     else:
+#         active = False
+
+
+# # using break to exit a loop
+# prompt = "Please enter the city that you have visited\n"
+# prompt += "Please enter 'quit' when you finished\n"
+
+# while True:
+#     city = input(prompt)
+#     if city == 'quit':
+#         break
+#     else:
+#         print(f"You have visited {city}\n")
+
+# # Using continue in a loop
+# current_number = 0
+# while current_number <= 10:
+#     current_number += 1
+#     if current_number%2 == 0:
+#         continue
+#     print(current_number)
+
+# # exercise 7-5
+# age = 0
+# prompt = "What is your age? \n"
+# prompt += "type 'quit' to quit"
+# age = input(prompt)
+# age = int(age)
+# if
