6a5827c1733975634527872c9fe097cb0d779485
Fredchen <fredchen001@outlook.com>
2021-11-17 16:29:48 -0800

    4

--- 11_16.py
+++ 11_16.py
@@ -1,2 +1,20 @@
 # Storing your functions in Modules
 #  代码实现过于复杂，详看书第150页起
+
+
+# Classes 
+# Creating a Dog class
+class Dog:
+    """A Simple attempt to model a dog"""
+    def __init__(self,name,age):
+        """Initialize name and age attributes."""
+        self.name = name
+        self.age = age
+    
+    def sit(self):
+        """Simulate a dog sitting in response to a command"""
+        print(f"{self.name} is now sitting")
+
+    def roll_over(self):
+        """Simulate roll over in response to a command"""
+        print(f"{self.name} is rolling over!")
